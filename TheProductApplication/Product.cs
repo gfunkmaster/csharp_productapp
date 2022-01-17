6acd3757f30266915619708bc3d14486b544b893
gfunkmaster <nurkicelmin@hotmail.com>
2022-01-17 11:56:15 +0100

    first commit

---
+++ TheProductApplication/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TheProductApplication
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+    }
+}
---
+++ TheProductApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheProductApplication
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

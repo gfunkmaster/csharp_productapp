fe0c22b0a75404514c2f8508886e982ed6c9247c
gfunkmaster <nurkicelmin@hotmail.com>
2022-01-17 11:56:43 +0100

    Exercise done

--- TheProductApplication/Product.cs
+++ TheProductApplication/Product.cs
@@ -3,8 +3,47 @@ namespace TheProductApplication
 {
     public class Product
     {
-        public Product()
+        //input variables to store values given by the user
+        private string name; // name of the product as text
+        private double price; // price as reel number (whit or whitout decimals )
+        private int count; // quantity of the product as whole number
+
+        //Read the name of the product from the Prompt Window
+        public void ReadName()
         {
+            Console.WriteLine("Name of the product?"); // Writes the text to Console window
+            name = Console.ReadLine();
+            Console.WriteLine($"The product's name is: {name}");
+        }
+
+        public void ReadPrice()
+        {
+            Console.WriteLine($"What's the price of the {name}?"); // writes the text to console window and using the name og the item
+            string priceName = Console.ReadLine(); // so we can write in the price
+            price = Double.Parse(priceName);    // parse, convering it so it have same type
+            Console.WriteLine($"The price of {name} is ${price}"); // writing it out on console
+        }
+
+        // Ask the user to feed in the qunatity of the product as whole number
+        public void ReadCount()
+        {
+            Console.WriteLine($"How many {name}'s are there?");
+            //User's input is a text(ex 34)
+            string strCount = Console.ReadLine();
+
+            //convert the text to its corresponding number ex 34 (2cgars) ---> 34 (number 34)
+            count = int.Parse(strCount);
+
+            //Confirm the input, toString converts the count to text: ex 34 ()number ---> "34" (chars)
+            Console.WriteLine($"The no of {name}'s:" + count.ToString());
+        }
+
+        // Calculate the total price = count * price, Dispaly the result to the user
+        public void CalculateTotalPrice()
+        {
+            double totalPrice = count * price;
+            //display the result to the user; put a space beetween thetwo words
+            Console.WriteLine("Total price for " + count.ToString() + " units of " + name + " = " + totalPrice);
         }
     }
 }
--- TheProductApplication/Program.cs
+++ TheProductApplication/Program.cs
@@ -6,7 +6,18 @@ namespace TheProductApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Testing my first C# program at Malmö University!");
+            Console.Title = "My First C# Program at Malmö University";
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Product product = new Product(); //Create an object of the Product class
+            product.ReadName(); // call (active) a method of the object
+            product.ReadPrice();// call (active) a method of the object
+            product.ReadCount();
+            product.CalculateTotalPrice();
+
         }
     }
 }
